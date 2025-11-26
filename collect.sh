7324c9586f4379428eee8544d24bb66cb4bb5525
Dongxu Chen <1625326705@qq.com>
2025-11-26 12:38:55 +0800

    update collect.sh

--- collect.sh
+++ collect.sh
@@ -3,8 +3,22 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate l3mvn
 
+HEIGHTS=(1.0 1.2 1.4)
 FOVS=(75 90 105 120)
-HEIGHTS=(0.6 0.9 1.2 1.5)
+
+total_scenes=80 # train scenes
+slices=$(( ${#HEIGHTS[@]} * ${#FOVS[@]} ))
+n=$(( total_scenes / slices ))
+
+# max processes on single 3090
+if [ "$n" -gt 6 ]; then
+    n=6
+elif [ "$n" -lt 1 ]; then
+    n=1
+fi
+
+echo "Total slices: $slices"
+echo "Number of processes: $n"
 
 run_id=0
 
@@ -19,11 +33,11 @@ for h in "${HEIGHTS[@]}"; do
             --load pretrained_models/llm_model.pt \
             --use_gtsem 0 \
             --num_local_steps 10 \
-            -n 5 \
+            -n $n \
             --num_eval_episodes 100 \
             --camera_height "$h" \
             --hfov "$f" \
-            --scene_slice_total 16 \
+            --scene_slice_total $slices \
             --scene_slice_index "$run_id"
 
         run_id=$((run_id + 1))
